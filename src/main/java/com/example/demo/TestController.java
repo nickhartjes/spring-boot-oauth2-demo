23e8090aad0aebaa7c3d2256beea6564c7c2e72c
Nick Hartjes <info@nickhartjes.nl>
2020-06-08 11:36:30 +0200

    First commit

---
+++ src/main/java/com/example/demo/TestController.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/api")
+    String api() {
+        return "Made it to protected api on resource server!";
+    }
+}
